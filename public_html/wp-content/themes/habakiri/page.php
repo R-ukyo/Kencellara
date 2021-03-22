dbbd89447ddc72c786050b078d68577b3909b5eb
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-03-22 18:15:30 +0900

    breadcrumbsをコメントに

--- public_html/wp-content/themes/habakiri/page.php
+++ public_html/wp-content/themes/habakiri/page.php
@@ -26,7 +26,7 @@
 			<div class="col-md-9">
 				<main id="main" role="main">
 
-					<?php get_template_part( 'modules/breadcrumbs' ); ?>
+					<?php #get_template_part( 'modules/breadcrumbs' ); ?>
 					<?php while ( have_posts() ) : the_post(); ?>
 						<?php get_template_part( 'content', 'page' ); ?>
 					<?php endwhile; ?>
