cc1d77518a154810dd4c7a2f351177219b8eb462
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-04-10 12:31:02 +0900

    フッター仮完成

--- public_html/wp-content/themes/habakiri/footer.php
+++ public_html/wp-content/themes/habakiri/footer.php
@@ -9,42 +9,36 @@
  * License URI: license.txt
  */
 ?>
+<?php
+	add_action('wp_enqueue_scripts', wp_enqueue_style('footer-style', get_template_directory_uri() . '/css/footer.css', array('common-style')));
+?>
 		<?php do_action( 'habakiri_after_contents_content' ); ?>
 	<!-- end #contents --></div>
 	<footer id="footer" class="footer">
-		<?php do_action( 'habakiri_before_footer_content' ); ?>
-		
-		<?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
-		<div class="footer-widget-area">
-			<div class="container">
-				<div class="row">
-					<?php dynamic_sidebar( 'footer-widget-area' ); ?>
-				<!-- end .row --></div>
-			<!-- end .container --></div>
-		<!-- end .footer-widget-area --></div>
-		<?php endif; ?>
-
-		<?php if ( has_nav_menu( 'social-nav' ) ) : ?>
-		<div class="social-nav">
-			<div class="container">
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'social-nav',
-					'fallback_cb'    => '',
-					'depth'          => 1
-				) );
-				?>
-			<!-- end .container --></div>
-		<!-- end .social-nav --></div>
-		<?php endif; ?>
-
-		<div class="copyright">
-			<div class="container">
-				<?php get_template_part( 'modules/copyright' ); ?>
-			<!-- end .container --></div>
-		<!-- end .copyright --></div>
-		<?php do_action( 'habakiri_after_footer_content' ); ?>
-	<!-- end #footer --></footer>
+		<div id="footer-menu">
+		  <div id="blog-title-content">
+	      <a href="https://www.kencellara.com/">
+	        <img class="hatena-fotolife" title="ケンチェラーラ." src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/ken_chan_bike/20210109/20210109122234.jpg" data-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/ken_chan_bike/20210109/20210109122234.jpg" alt="ケンチェラーラフッター." style="height: 50px;" loading="lazy" />
+	      </a>
+	    </div>
+	    <nav>
+	      <div class="footer-links">
+	        <ul>
+	          <li>
+	            <a href="https://www.kencellara.com/kence_work">お仕事のご依頼</a>
+	          </li>
+	          <li>
+	            <a href="https://www.kencellara.com/kence_work">お問い合わせ</a>
+	          </li>
+	          <li>
+	            <a href="https://www.kencellara.com/privacy-policy">プライバシーポリシー</a>
+	          </li>
+	        </ul>
+	      </div>
+	    </nav>
+	    <p class="copyright">© 2020 kencellara.com All rights reserved.</p>
+		</div>
+	</footer>
 <!-- end #container --></div>
 <?php wp_footer(); ?>
 </body>
