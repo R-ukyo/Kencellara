b0d37f1e2625abda368c92b9452046daf22b9a63
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-05-08 00:23:36 +0900

    記事下スタイル調整

--- public_html/wp-content/themes/habakiri/content.php
+++ public_html/wp-content/themes/habakiri/content.php
@@ -28,7 +28,7 @@
 		comments_template( '', true );
 	}
 	*/ ?>
-	<div class="kenceWorkWindowHeader">お問い合わせフォーム</div>
+	<div class="kenceWorkWindowHeader belowArticleHeader">お問い合わせフォーム</div>
 	<div class="kenceWorkWindow">
 	  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf7z8zl1FGl9_pWK9EOQBTbHDTRJ3hd8yIlKoSIPhl4-LbGfg/viewform?embedded=true" width="100%" height="850" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
 	</div>
