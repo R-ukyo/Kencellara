ba1d4ba7cfdc91e3f1f9687becb66a25486925b6
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-05-19 14:35:47 +0900

    サイドバーにピックアップ記事実装

---
+++ public_html/wp-content/themes/habakiri/modules/category_articles.php
@@ -0,0 +1,22 @@
+<?php
+  $args = array(
+    'posts_per_page' => 5,
+    'category_name' => 'ramen'
+  );
+  $posts = get_posts( $args );
+  foreach ( $posts as $post ):
+  setup_postdata( $post );
+  $cats = get_the_category();
+?>
+<div class="sb_categoryArticle sb_article">
+  <div class="sb_categoryArticlesThumbnail sb_articleThumbnail">
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+  </div>
+  <div class="sb_categoryArticlesTitle sb_articleTitle">
+    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+  </div>
+</div>
+<?php
+  endforeach;
+  wp_reset_postdata(); // 直前のクエリを復元する
+?>
---
+++ public_html/wp-content/themes/habakiri/modules/popular_articles.php
@@ -0,0 +1,22 @@
+<?php
+  $args = array(
+    'posts_per_page' => 5,
+    'category_name' => 'ise'
+  );
+  $posts = get_posts( $args );
+  foreach ( $posts as $post ):
+  setup_postdata( $post );
+  $cats = get_the_category();
+?>
+<div class="sb_popularArticle sb_article">
+  <div class="sb_popularArticlesThumbnail sb_articleThumbnail">
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+  </div>
+  <div class="sb_popularArticlesTitle sb_articleTitle">
+    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+  </div>
+</div>
+<?php
+  endforeach;
+  wp_reset_postdata(); // 直前のクエリを復元する
+?>
