01f5ca8313d012b96b7fd5ab7b0942c4db35b01d
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-05-29 03:01:30 +0900

    お問い合わせフォームに説明つける

--- public_html/wp-content/themes/habakiri/content.php
+++ public_html/wp-content/themes/habakiri/content.php
@@ -28,11 +28,18 @@
 		comments_template( '', true );
 	}
 	*/ ?>
-	<div class="kenceWorkWindowHeader belowArticleHeader">お問い合わせフォーム</div>
-	<div class="kenceWorkWindow">
-	  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf7z8zl1FGl9_pWK9EOQBTbHDTRJ3hd8yIlKoSIPhl4-LbGfg/viewform?embedded=true" width="100%" height="850" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+	<div class="kenceWorkWindowZone">
+		<div class="kenceWorkWindowHeader belowArticleHeader">お問い合わせフォーム</div>
+		<div class="kenceWorkWindowExplain">
+			<p>取材依頼・ケンチェ飯に行って欲しいお店・一緒にご飯を食べたい！などのお誘いを常時受け付けております。お気軽にご連絡下さい。</p>
+		</div>
+		<div class="kenceWorkWindow">
+		  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf7z8zl1FGl9_pWK9EOQBTbHDTRJ3hd8yIlKoSIPhl4-LbGfg/viewform?embedded=true" width="100%" height="850" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+		</div>
 	</div>
-	<div class="kencemeshiSupportHeader">日頃よりご支援頂き誠にありがとうございます。</div>
-	<script src="https://codoc.jp/js/cms.js" data-css="red-square" charset="UTF-8" defer="defer"></script>
-	<div id="codoc-entry-Aly1W7g5Yg" class="codoc-entries" data-without-body="1" data-support-button-text="ケンチェ飯を支援する" data-show-like="0" data-show-powered-by="0" data-support-message="頂戴しましたご支援は、サイトの運営費に補填させて頂いております。"></div>
+	<div class="kencemeshiSupportZone">
+		<div class="kencemeshiSupportHeader">日頃よりご支援頂き誠にありがとうございます。</div>
+		<script src="https://codoc.jp/js/cms.js" data-css="red-square" charset="UTF-8" defer="defer"></script>
+		<div id="codoc-entry-Aly1W7g5Yg" class="codoc-entries" data-without-body="1" data-support-button-text="ケンチェ飯を支援する" data-show-like="0" data-show-powered-by="0" data-support-message="頂戴しましたご支援は、サイトの運営費に補填させて頂いております。"></div>
+	</div>
 </article>
